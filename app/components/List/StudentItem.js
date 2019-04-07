9a157610cf75f467fa05022ddaa0103e76528863
Nabeel K <nabeel.kottol@gmail.com>
2019-04-07 15:46:51 +0530

    refactor student tab

--- app/components/List/StudentItem.js
+++ app/components/List/StudentItem.js
@@ -1,8 +1,98 @@
+import React from "react";
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import {
+  Screen,
+  capitalizeFirstLetter,
+  makePhoneCallWith,
+} from "utils";
+import moment from 'moment';
+import Images from '../../assets/images';
 
+const letterCircleSize = Screen.width * .08;
 
 
 const StudentItem = (props) => {
+  const { item } = props;
+  const { name, category, batchName, phoneNumber, created, circleColor } = item;
 
+  return (
+    <View style={styles.rowContainer}>
+      <View >
+        <View style={[styles.letterCircle, { backgroundColor: circleColor }]}>
+          <Text style={styles.letterText}>
+            {name[0].toUpperCase()}
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.infoColomn}>
+        <View style={styles.mainRow}>
+          <Text style={{ fontSize: 18, fontWeight: '500' }}>
+            {capitalizeFirstLetter(name.length > 18 ? `${name.slice(0, 18).trim()}...` : name.trim())}
+          </Text>
+
+          <Text style={styles.dateText}>
+            {/* No Data Available from API */}
+            {/* moment(postedOn, "DD/MM/YYYY").fromNow() */ "12/08/2019"}
+          </Text>
+        </View>
+
+        {this.renderTextRow(category)}
+        {this.renderTextRow(batchName)}
+      </View>
+
+      <View style={styles.actionColomn}>
+        <TouchableOpacity onPress={() => console.log("Add dropdown menu")}>
+          <Image resizeMode='contain' source={Images.menu} style={styles.menuImage} />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => makePhoneCallWith(phoneNumber)}>
+          <Image resizeMode='contain' source={Images.phoneCall} style={styles.callImage} />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
+renderTextRow = (data) => (
+  <View style={styles.textRow}>
+    <Text>
+      {data}
+    </Text>
+  </View>
+);
+
 export default StudentItem;
+
+const styles = StyleSheet.create({
+  rowContainer: { flex: 1, flexDirection: 'row', padding: 8 },
+  letterCircle: {
+    height: letterCircleSize,
+    width: letterCircleSize,
+    borderRadius: letterCircleSize / 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 8,
+  },
+  letterText: { color: 'white', fontSize: 18, fontWeight: 'bold' },
+  infoColomn: { padding: 8, flex: 1 },
+  mainRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginVertical: 2,
+    alignItems: 'center',
+    paddingRight: 24,
+  },
+  dateText: { fontSize: 12, color: 'grey' },
+  textRow: { marginVertical: 2 },
+  actionColomn: { justifyContent: 'space-around' },
+  menuImage: { height: 16, width: 16, tintColor: 'grey' },
+  callImage: { height: 16, width: 16 },
+});
--- app/components/StudentsTab.js
+++ app/components/StudentsTab.js
@@ -3,94 +3,24 @@ import {
   View,
   FlatList,
   ActivityIndicator,
-  Text,
-  Image,
-  TouchableOpacity,
-  Linking,
-  Platform,
-  Alert,
 } from "react-native";
-import { getRandomColor, Screen, capitalizeFirstLetter, makePhoneCallWith } from "utils";
-import moment from 'moment';
-import Images from '../assets/images';
-
+import { getRandomColor } from "utils";
+import StudentItem from "./List/StudentItem";
 import data from "./students.json";
 class StudentsTab extends React.Component {
 
   constructor(props) {
     super(props);
 
-    let students = data.dataList.map((item, index, arr) => {
+    let students = data.dataList.map((item) => {
       item.circleColor = getRandomColor();
 
       return item;
     });
 
-
     this.state = {
       students
     };
-  }
-
-  renderTextRow = (data) => (
-    <View style={{ marginVertical: 2 }}>
-      <Text>
-        {data}
-      </Text>
-    </View>
-  );
-
-  renderItem = (item, index) => {
-    console.log("rendering...");
-    const { name, category, batchName, phoneNumber, created, circleColor } = item;
-    const letterCircleSize = Screen.width * .08;
-
-    return (
-      <View style={{ flex: 1, flexDirection: 'row', padding: 8 }}>
-
-        <View >
-          <View style={[{ height: letterCircleSize, width: letterCircleSize, borderRadius: letterCircleSize / 2, justifyContent: 'center', alignItems: 'center', margin: 8 }, { backgroundColor: circleColor }]}>
-            <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
-              {name[0].toUpperCase()}
-            </Text>
-          </View>
-        </View>
-
-        <View style={{ padding: 8, flex: 1 }}>
-
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginVertical: 2, alignItems: 'center', paddingRight: 24 }}>
-
-            <Text style={{ fontSize: 18, fontWeight: '500' }}>
-              {capitalizeFirstLetter(name.length > 18 ? `${name.slice(0, 18).trim()}...` : name.trim())}
-            </Text>
-
-            <Text style={{ fontSize: 12, color: 'grey' }}>
-              {/* No Data Available from API */}
-              {/* moment(postedOn, "DD/MM/YYYY").fromNow() */ "12/08/2019"}
-            </Text>
-
-          </View>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <View>
-              {this.renderTextRow(category)}
-
-              {this.renderTextRow(batchName)}
-            </View>
-          </View>
-
-        </View>
-        <View style={{ justifyContent: 'space-around' }}>
-          <TouchableOpacity onPress={() => console.log("Add dropdown menu")}>
-            <Image resizeMode='contain' source={Images.menu} style={{ height: 16, width: 16, tintColor: 'grey' }} />
-          </TouchableOpacity>
-
-          <TouchableOpacity onPress={() => makePhoneCallWith(phoneNumber)}>
-            <Image resizeMode='contain' source={Images.phoneCall} style={{ height: 16, width: 16 }} />
-          </TouchableOpacity>
-
-        </View>
-      </View>
-    );
   }
 
   render() {
@@ -99,7 +29,7 @@ class StudentsTab extends React.Component {
       <View style={{ flex: 1 }}>
         <FlatList
           data={students}
-          renderItem={({ item, index }) => this.renderItem(item, index)
+          renderItem={({ item, index }) => <StudentItem item={item} />
           }
           keyExtractor={item => `${item.attendeeId}`}
         />
